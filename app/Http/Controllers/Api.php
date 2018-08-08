b4bda4f665757ad83fd4e8c4f478f815bf31934d
kangpenglin <763722424@qq.com>
2018-08-08 01:48:23 +0000

    third

--- app/Http/Controllers/Api.php
+++ app/Http/Controllers/Api.php
@@ -1,10 +1,30 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\image;
+use App\text;
 use Illuminate\Http\Request;
 
 class Api extends Controller
 {
-    //
+	public function image(Request $request)
+	{
+		$img=json_decode($request->input('img'));
+		var_dump($img);
+		if(count($img)==0)
+		{
+			return response()->json(
+				[
+				"code"=>"400","msg"=>"argument error"]);
+		}
+		$arr=array();
+		foreach ($img as $i)
+		{
+			$arr[]=$i;
+		}
+	       	
+		return response()->json(
+			["code"=>"200","msg"=>$arr]
+		);
+	}
 }
